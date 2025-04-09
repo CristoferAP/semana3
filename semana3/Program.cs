04765ef8e4df35a5e8a4dea6fad951365dc70e7d
Cristofer <cristoferasencios@gmail.com>
2025-04-09 10:03:55 -0500

    primer commit

---
+++ semana3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido al curso de c#");
+            Console.ReadKey();
+        }
+    }
+}
